5d414b83f86f8247730dd6fbb2a1868832d6a2d3
Nampa <henryyy3@hotmail.com>
2020-07-28 20:09:43 +0800

    Initial skeleton with basic methods outlined.

---
+++ src/BankSystem.java
@@ -0,0 +1,26 @@
+public class BankSystem {
+
+    public BankSystem() {
+
+    }
+
+    public int checkBalance() {
+
+        return -1;
+    }
+
+    public void deposit() {
+
+    }
+
+    public void withdraw() {
+
+    }
+
+    public void menu() {
+
+    }
+
+
+
+}
