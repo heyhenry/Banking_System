0027b1e902a2624499001398ad282873d206aa61
Nampa <henryyy3@hotmail.com>
2020-07-28 20:55:21 +0800

    Implemented switch statement wrapped in a do-while loop for solely user input driven action.

--- src/BankSystem.java
+++ src/BankSystem.java
@@ -36,5 +36,62 @@ public class BankSystem {
 
     }
 
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        char choice;
+
+        do {
+
+            System.out.println("\nUnited Banking Systems\n");
+            System.out.println("=============================");
+
+            System.out.println("A - Check Balance");
+            System.out.println("B - Deposit");
+            System.out.println("C - Withdraw");
+            System.out.println("D - Return Menu");
+
+            System.out.println("=============================");
+            System.out.println("\nChoose an option provided on the menu: ");
+            choice = sc.nextLine().charAt(0);
+
+            switch(Character.toUpperCase(choice)) {
+
+                case 'A' :
+
+                    System.out.println("Check Balance");
+                    break;
+
+                case 'B' :
+
+                    System.out.println("Deposit");
+                    break;
+
+                case 'C' :
+
+                    System.out.println("Withdraw");
+                    break;
+
+                case 'D' :
+
+                    System.out.println("Return Menu");
+                    break;
+
+                case 'X' :
+
+                    System.exit(0);
+
+                default:
+
+                    System.out.println("Invalid input! Returning to menu...");
+                    break;
+
+            }
+
+        } while (Character.toUpperCase(choice) != 'X');
+
+    }
+
 
 }
